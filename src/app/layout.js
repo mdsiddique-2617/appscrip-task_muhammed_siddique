fb31e033dad698f9b727ba79dd8260f9f42ae902
mdsiddique-2617 <siddiquedotmax@gmail.com>
2025-05-10 18:10:54 +0530

    final commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import FilterSection from "./components/SidebarFilters";
+// import FilterSection from "./components/SidebarFilters";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
