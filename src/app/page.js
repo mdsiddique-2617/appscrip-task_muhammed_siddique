31480942036d9c3edb35ba11d5316b27c2b8a049
mdsiddique-2617 <siddiquedotmax@gmail.com>
2025-05-10 17:39:29 +0530

    first and final commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,94 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import ShopStyles from './styles/shop.module.css';
+import { FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft } from "react-icons/fa6";
+import React, { useState } from 'react';
+import ProductCard from './components/ProductCard';
+import ProductGrid from './components/ProductGrid';
+import SidebarStyles from "./styles/sidebar.module.css";
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+// import React, { useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const [rating, setRating] = useState("");
+  const handleRatingChange = (event) => {
+    setRating(event.target.value);
+  };
+  console.log(rating);
+
+  const sections = [
+    { OCCASION: ["Casual", "Formal", "Party"] },
+    { WORK: ["Office", "Outdoor", "Creative"] },
+    { FABRIC: ["Cotton", "Silk", "Denim"] },
+    { SEGMENT: ["Men", "Women", "Kids"] },
+    { SUITABLE_FOR: ["Summer", "Winter", "All Seasons"] },
+    { RAW_MATERIALS: ["Organic", "Recycled", "Synthetic"] },
+    { PATTERN: ["Striped", "Solid", "Printed"] }
+  ];
+
+  const [status, setStatus] = useState(false);
+  const toggleStatus = () => {
+    setStatus(!status);
+  }
+
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  }
+
+  return (
+    <div className={ShopStyles.overallShop}>
+      <div className={ShopStyles.discover}>
+        <h1>DISCOVER OUR PRODUCTS</h1>
+        <p>Explore our wide range of products designed to meet your needs. Explore our wide range of products designed to meet your needs.</p>
+      </div>
+      <div className={ShopStyles.overallFilter}>
+        <div>
+          <h4>20 items</h4>
+          <div>
+            {isChecked ? <FaChevronLeft /> :  <FaChevronRight />}
+            <label htmlFor='showFilter' onClick={()=>handleCheckboxChange()}>Show Filter</label>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <select name="rating" id="rating-select" className={ShopStyles.grade_select} value={rating} onChange={handleRatingChange} >
+            <option value="" onClick={() => handleRatingChange()}>-- SELECT A RATING --</option>
+            <option value="recommended" onClick={() => handleRatingChange()}>RECOMMENDED</option>
+            <option value="best" onClick={() => handleRatingChange()}>BEST</option>
+            <option value="good" onClick={() => handleRatingChange()}>GOOD</option>
+            <option value="average" onClick={() => handleRatingChange()}>AVERAGE</option>
+            <option value="poor" onClick={() => handleRatingChange()}>POOR</option>
+          </select>
+        </div>
+      </div>
+      <div className={SidebarStyles.sidebar_container}>
+        <input type='checkbox' className={SidebarStyles.check} id="showFilter" />
+        <div className={`${SidebarStyles.sidebar} ${isChecked ? SidebarStyles.active : ""}`}>
+          {sections.map((section, index) => {
+            const key = Object.keys(section)[0];
+            return (
+              <div key={index} className={SidebarStyles.filter_group}>
+                <div className={SidebarStyles.filter_header}>
+                  <h3>{key}</h3>
+                  {status ? <FaChevronUp onClick={() => toggleStatus()} /> : <FaChevronDown onClick={() => toggleStatus()} />}
+                </div>
+                <ul>
+                  {section[key].map((value, i) => (
+                    <div className={`${SidebarStyles.filter_option} ${status ? SidebarStyles.active : ""}`} key={i}>
+                      <input type="checkbox" id={value} name={value} value={value} />
+                      <label htmlFor={value}>{value}</label>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+        <ProductGrid rating={rating} />
+      </div>
+
     </div>
   );
 }
